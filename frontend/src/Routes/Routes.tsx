1965d1c5b582ca33a632021b81ba7dd8662f7aec
Raeezadams123@gmail.com <Samsung123786@>
2025-01-12 20:22:56 +0200

    Added routes

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -1,27 +1,50 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
-import HomePage from '../Pages/HomePage/HomePage';
-import SearchPage from '../Pages/SearchPage/SearchPage';
-import ComapnyPage from '../Pages/CompanyPage/ComapnyPage';
-import CompanyProfile from '../Components/CompanyProfile/CompanyProfile';
-import IncomeStatement from '../Components/IncomeStatement/IncomeStatement';
-import DesignPage from '../Pages/DesignPage/DesignGuide';
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import HomePage from "../Pages/HomePage/HomePage";
+import CompanyPage from "../Pages/CompanyPage/CompanyPage";
+import SearchPage from "../Pages/SearchPage/SearchPage";
+import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
+import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
+import DesignGuide from "../Pages/DesignGuide/DesignGuide";
+import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
+import HistoricalDividend from "../Components/HistoricalDividend/HistoricalDividend";
+import CashflowStatement from "../Components/CashflowStatement/CashflowStatement";
+import LoginPage from "../Pages/LoginPage/LoginPage";
+import RegisterPage from "../Pages/RegisterPage/RegisterPage";
+import ProtectedRoute from "./ProtectedRoute";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
-      { path: '', element: <HomePage /> },
-      { path: 'search', element: <SearchPage /> },
-      { path: "design-guide", element: <DesignPage/>},
-      { path: 'company/:ticker', 
-        element: <ComapnyPage />,
-      children: [
-        { path: 'company-profile', element: <CompanyProfile /> },
-        { path: 'income-statement', element: <IncomeStatement /> },
-
-      ] },
+      { path: "", element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
+      {
+        path: "search",
+        element: (
+          <ProtectedRoute>
+            <SearchPage />
+          </ProtectedRoute>
+        ),
+      },
+      { path: "design-guide", element: <DesignGuide /> },
+      {
+        path: "company/:ticker",
+        element: (
+          <ProtectedRoute>
+            <CompanyPage />
+          </ProtectedRoute>
+        ),
+        children: [
+          { path: "company-profile", element: <CompanyProfile /> },
+          { path: "income-statement", element: <IncomeStatement /> },
+          { path: "balance-sheet", element: <BalanceSheet /> },
+          { path: "cashflow-statement", element: <CashflowStatement /> },
+          { path: "historical-dividend", element: <HistoricalDividend /> },
+        ],
+      },
     ],
   },
 ]);
